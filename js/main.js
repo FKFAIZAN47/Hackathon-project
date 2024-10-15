4d7092aa03cdb2f5c73cb3eba54dfb65d4530cd0
L-Patel0365 <lavgithub365@gmail.com>
2024-10-14 22:21:03 -0400

    Update main.js
    I'm Adding Javascript code for the portfolio.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,75 @@
 // Variables
-const player = new Plyr("video");
+const player = new Plyr('video');
 const navLinks = document.querySelectorAll("nav a");
 
 // Functions
 function smoothScroll(e) {
-  e.preventDefault();
-
+  e.preventDefault(); 
+  
   const sectionID = this.getAttribute("href").substring(1);
   const targetSection = document.getElementById(sectionID);
-
+  
   // Smooth scroll to the target section
   targetSection.scrollIntoView({
     behavior: "smooth",
-    block: "start",
+    block: "start"
   });
 }
 
 // Event Listeners
 
 document.addEventListener("DOMContentLoaded", () => {
-  navLinks.forEach((link) => {
+  navLinks.forEach(link => {
     link.addEventListener("click", smoothScroll);
   });
 });
+
+//
+
+const portfolioCon = document.querySelector("#portfolio-con");
+
+const portfolios = [
+{name: "Abdullh Sultan", pLink: "abdullhsultan.com"},
+{name: "Aiden Wotton", pLink: "aidenwotton.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Bhuvnish Arora", pLink: "bhuvnisharora.com"}
+];
+
+portfolios.forEach((portfolio) => {
+  const portfolioDiv = document.createElement("div");
+  portfolioDiv.classList.add("student-box");
+
+  const portfolioName = document.createElement("h3");
+  portfolioName.textContent = portfolio.name;
+
+  const portfolioLink = document.createElement("a");
+  portfolioLink.href = `https://${portfolio.pLink}`;
+  portfolioLink.target = "_blank";
+  portfolioLink.textContent = portfolio.pLink;
+
+  portfolioDiv.appendChild(portfolioName);
+  portfolioDiv.appendChild(portfolioLink);
+  portfolioCon.appendChild(portfolioDiv);
+});
+
+
