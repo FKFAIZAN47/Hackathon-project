b2a5f7bd71f552995008ef84b8983431976a6b0f
L-Patel0365 <lavgithub365@gmail.com>
2024-10-14 22:25:33 -0400

    Update main.js
    I'm adding Javascript code for the portfolio page.

--- js/main.js
+++ js/main.js
@@ -38,3 +38,50 @@ gsap.from(".capstone-title, .capstone-desc", {
   duration: 1,
   delay: 1,
 });
+
+
+const portfolioCon = document.querySelector("#portfolio-con");
+
+const portfolios = [
+{name: "Abdullh Sultan", pLink: "abdullhsultan.com"},
+{name: "Aiden Wotton", pLink: "aidenwotton.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Apple Grace Geaslan", pLink: "applegracegeaslan.com"},
+{name: "Bhuvnish Arora", pLink: "bhuvnisharora.com"}
+];
+
+portfolios.forEach((portfolio) => {
+  const portfolioDiv = document.createElement("div");
+  portfolioDiv.classList.add("student-box");
+
+  const portfolioName = document.createElement("h3");
+  portfolioName.textContent = portfolio.name;
+
+  const portfolioLink = document.createElement("a");
+  portfolioLink.href = `https://${portfolio.pLink}`;
+  portfolioLink.target = "_blank";
+  portfolioLink.textContent = portfolio.pLink;
+
+  portfolioDiv.appendChild(portfolioName);
+  portfolioDiv.appendChild(portfolioLink);
+  portfolioCon.appendChild(portfolioDiv);
+});
